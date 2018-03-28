0dd0902b591a7fbd061ebf05976d642252309acb
Sille Van Landschoot <info@sillevl.be>
2018-03-28 23:33:15 +0200

    add welcome image with parallax

---
+++ components/Introduction.vue
@@ -0,0 +1,9 @@
+<template>
+    <v-parallax src="/img/background.png" style="width: 100%">
+        <v-layout column align-center justify-center>
+            <h4 class="white--text">99 Bugs</h4>
+            <h1 class="white--text">LanParty</h1>
+            <img src="/img/viking.png" alt="" height="200px;">
+        </v-layout>
+    </v-parallax>
+</template>
